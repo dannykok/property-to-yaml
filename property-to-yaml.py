9d015f5856bf85a25b598daa4ef7747cbafed3bc
dannykok <dannykok1@gmail.com>
2023-06-04 04:18:04 +0800

    initial commit

---
+++ property-to-yaml.py
@@ -0,0 +1,24 @@
+import sys, yaml
+
+# Read the input and output filenames from the command line arguments
+input_filename = sys.argv[1]
+output_filename = sys.argv[2]
+
+# Open the input file and read its contents
+with open(input_filename, 'r') as f:
+    env_contents = f.read()
+
+# Split the contents into lines and remove any empty lines or comments
+env_lines = [line.strip() for line in env_contents.split('\n') if line.strip() and not line.startswith('#')]
+
+# Convert the lines into a dictionary
+env_dict = {}
+for line in env_lines:
+    key_value_pair = line.split('=')
+    key = key_value_pair[0].strip()
+    value = '='.join(key_value_pair[1:]).strip().strip('"').strip("'")
+    env_dict[key] = value
+
+# Convert the dictionary to YAML and write it to the output file
+with open(output_filename, 'w') as f:
+    yaml.dump(env_dict, f, default_flow_style=False)
